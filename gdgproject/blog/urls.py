7c163a12320953dbc41371db4c992f7b4baf65cb
Arsema <arsemamenberu16@gmail.com>
2026-02-04 02:51:29 -0800

    Change

--- gdgproject/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import hello_blog, WelcomeView, all_posts
-
-urlpatterns = [
-    path('', hello_blog, name='root'),                # <-- root URL
-    path('home/', hello_blog, name='home'),
-    path('welcome/', WelcomeView.as_view(), name='welcome'),
-    path('posts/', all_posts, name='all_posts'),
-]
